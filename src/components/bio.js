f2532ba76570108c98a6c879801319ccec20fb4e
niemisami <sami.nieminen@madbooster.com>
2020-01-19 10:49:44 +0200

    Update latest helmet plugins

--- src/components/bio.js
+++ src/components/bio.js
@@ -15,7 +15,7 @@ const Bio = () => {
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
+          fixed(width: 150, height: 150) {
             ...GatsbyImageSharpFixed
           }
         }
